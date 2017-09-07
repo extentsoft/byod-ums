517aac08685cec541729412538740a5eaf51d417
macbook <macbook@macbooks-MacBook-Pro.local>
2017-09-08 02:05:48 +0700

    Added Q&A.xlsx

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # byod-ums
 BYOD-User Management System
+
+# setup
+1. npm install
+
+# run
+$ DEBUG=byod-ums:* npm start
+  
--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var users = require('./routes/users');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,8 +23,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', index);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
