3c4054604ae02b9ba9abcc06fd48944a9441337a
tonghuntz <tonghuntz@hotmail.com>
2017-11-30 22:40:25 +0700

    Added listmac and updatemac

---
+++ routes/updatemac.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var pool = require('../modules/mssql').pool;
+var Request = require('tedious').Request;
+var router = express.Router();
+
+var deviceList = function(req,res,next){
+
+  var result = [];
+  pool.acquire(function(err, connection){
+    if(err){
+      console.error(err);
+      return;
+    }
+    console.log('Connection successful');
+
+    var request = new Request("UPDATE [AgileControllerDB].[dbo].[TSM_E_Account] SET [bindMac] = '"+req.param('macbind')+"' WHERE account = '"+req.param('accname')+"';", function(err, rowCount){
+
+	//    var request = new Request("SELECT '"+req.param('name')+"'", function(err, rowCount){
+
+      if(err){
+        console.error(err);
+        return;
+      }
+
+      req.resp = result;
+      connection.release();
+      next();
+    });
+
+    request.on('row', function(columns){
+      //result.push(columns);
+      columns.forEach(function(column){
+        if(column.value === null) {
+          console.log('NULL');
+        }
+        else{
+			result.push(column.value);
+          //console.log(column.value);
+        }
+      });
+    });
+    connection.execSql(request);
+  });
+
+
+}
+
+
+router.get('/', deviceList, function(req, res, next) {
+
+  res.send(req.resp);
+
+});
+
+module.exports = router;
+
+
