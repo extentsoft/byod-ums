3e259acc0742a6dcb52e0477b10faed6c97bb8a7
Thanakorn Piroonsith <thanakorn@extentsoft.com>
2017-11-30 22:26:02 +0700

    Merged reports

---
+++ config/dpis.js
@@ -0,0 +1,17 @@
+module.exports = {
+  development: {
+    connectionString: "fewfw",
+    username: "fwefw",
+    password: "fewfw"
+  },
+  test: {
+    connectString: "fewfw 123",
+    username: "Fewfwe",
+    password: "fwefw"
+  },
+  production: {
+    connectString: "fewfw",
+    username: "Fewfwe",
+    password: "fwefw"
+  }
+}
