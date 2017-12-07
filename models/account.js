eb169af80c90fd0e39b9a08c790f8403ee588a0e
Thanakorn Piroonsith <thanakorn@extentsoft.com>
2017-12-07 11:42:17 +0700

    Rewrote model/account

--- models/account.js
+++ models/account.js
@@ -1,33 +1,70 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-/*
-var Schema = mongoose.Schema;
-var passportLocalMongoose = require('passport-local-mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+var OpenLdap = require('../modules/openldap');
+var openldap = new OpenLdap();
 
-var Account = new Schema({
-  username: String,
-  password: String
-});
+var Account = function(){
+  var _id;
+  var email;
+  var password;
+  /*
+  this._id = '12021';
+  this.email = 'guest1@ums.com';
+  this.password = '123456';
+  this._id = _id;
+  this.email = email;
+  this.password = password;*/
 
-Account.plugin(passportLocalMongoose);
-module.exports = mongoose.model('Account', Account);
-*/
-
-
-var accountSchema = new Schema({
-  id: String,
-	username: String,
-	password: String,
-	email: String,
-	firstName: String,
-  lastName: String
-
-});
-
-accountSchema.methods.dudify = function(){
-  this.username = this.username + '-dude';
-  return this.username;
 }
+//findById(id, function(err, user))
+Account.prototype.findById = function(id, done){
+  //done(err, user);
+  done(null, this);
+};
 
-var Account = mongoose.model('Account', accountSchema);
-module.exports = Account;
+Account.prototype.findOne = function(opts, done){
+  console.log("prototype findOne : " +  opts.email);
+  console.log(this);
+  //done(err, user);
+
+
+  openldap.search(opts.email, function(entry){
+    if( entry instanceof Error){
+      console.log('Error');
+      done(entry,null);
+      return;
+    }
+    console.log('After error');
+    //console.log(entry);
+    this._id = entry.uid;
+    this._email = entry.mail;
+    this.password = '{SHA}{SSHA}pSwicOfZpLXwXRoSi0+22GlP+FXY8cxm';
+    //console.log(this);
+    return done(null,this);
+  });
+
+
+};
+
+// done = function(err,user)
+Account.prototype.test = function(opts, done){
+  openldap.test(opts, function(entry){
+    if( entry instanceof Error){
+      console.log('Error');
+      return done(new Error('Error'), null);
+    }
+    else{
+      console.log('not error');
+      //console.log(entry);
+      console.log(this.email);
+      this._id = entry.uid;
+      this.email = entry.uid;
+      this.password = entry.userPassword;
+      console.log('rewriting value');
+      console.log(this.email);
+
+      return done(null, this);
+    }
+  });
+};
+
+module.exports = new Account();
