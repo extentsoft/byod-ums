9f159e976fc69e1df0ebdf910ef01bd6788fa3d2
Thanakorn Piroonsith <thanakorn@extentsoft.com>
2017-12-07 13:09:47 +0700

    Cleaned up passport stuff in app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var flash = require('connect-flash');
 var bCrypt = require('bcrypt-nodejs');
 
-
+require('./config/passport_local');
 
 
 var index = require('./routes/index');
@@ -50,161 +50,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(require('express-session')({
-  secret: 'keyword',
-  resave: false,
-  saveUninitialized: false
+  secret: 'byodatexcise',
+  resave: true,
+  saveUninitialized: true
 }));
 app.use(passport.initialize());
+app.use(passport.session({secret: 'byodatexcise'}));
 app.use(flash());
-app.use(passport.session({secret: 'byodatexcise.go.th'}));
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Mongoose
-//mongoose.connect('mongodb://admin2:123456@ds127994.mlab.com:27994/ums');
-
-// Passport Configuration
-var Account = require('./models/account');
-
-/*
-passport.use(new LocalStrategy(Account.authenticate()));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
-*/
-passport.serializeUser(function(account, done) {
-  console.log('serializing user: ');
-  console.log(account);
-  done(null, account._id);
-
-});
-
-passport.deserializeUser(function(id, done) {
-  /*Account.findById(id, function(err, account) {
-    console.log('deserializing user:',account);
-    done(err, account);
-  });*/
-
-  done(null, {
-    _id:'nattawat_a',
-    username:'test1',
-    password:'123456',
-    email:'',
-    firstName:'Natthawa',
-    lastName:'Arun'
-  });
-});
-
-/* Account test
-user: test1
-pass: 123456
-*/
-passport.use('login', new LocalStrategy({
-    passReqToCallback : true
-  },
-  function(req, username, password, done) {
-    //console.log(username);
-    //console.log(password);
-
-
-
-    // change to ldap
-    if( username == 'test1' && password == "123456"){
-      var user = {
-        _id: 'nattawat_a',
-        name: 'natthawat_a',
-        phone: '1234444'
-      };
-      return done(null, user);
-    }
-    else{
-      return done(null, false, req.flash('message', 'User Not found.'));
-    }
-
-    // check in mongo if a user with username exists or not
-    /*
-    Account.findOne({ 'username' :  username },
-      function(err, account) {
-        // In case of any error, return using the done method
-        if (err)
-          return done(err);
-        // Username does not exist, log error & redirect back
-        if (!account){
-          console.log('User Not Found with username '+username);
-          return done(null, false,
-                req.flash('message', 'User Not found.'));
-        }
-        // User exists but wrong password, log the error
-        if (!isValidPassword(account, password)){
-          console.log('Invalid Password');
-          return done(null, false,
-              req.flash('message', 'Invalid Password'));
-        }
-        // User and password both match, return user from
-        // done method which will be treated like success
-        return done(null, account);
-      }
-    );
-    */
-}));
-
-var isValidPassword = function(account, password){
-  /*if( account.password == password) return true;
-  else return false;*/
-  return bCrypt.compareSync(password, account.password);
-}
-
-passport.use('signup', new LocalStrategy({
-            passReqToCallback : true // allows us to pass back the entire request to the callback
-        },
-        function(req, username, password, done) {
-
-            findOrCreateUser = function(){
-                // find a user in Mongo with provided username
-                Account.findOne({ 'username' :  username }, function(err, account) {
-                    // In case of any error, return using the done method
-                    if (err){
-                        console.log('Error in SignUp: '+err);
-                        return done(err);
-                    }
-                    // already exists
-                    if (account) {
-                        console.log('User already exists with username: '+username);
-                        return done(null, false, req.flash('message','User Already Exists'));
-                    } else {
-                        // if there is no user with that email
-                        // create the user
-                        var newAccount = new Account();
-
-                        // set the user's local credentials
-                        newAccount.username = username;
-                        //newAccount.password = password;
-                        newAccount.password = createHash(password);
-                        newAccount.email = req.param('email');
-                        newAccount.firstName = req.param('firstName');
-                        newAccount.lastName = req.param('lastName');
-
-                        // save the user
-                        newAccount.save(function(err) {
-                            if (err){
-                                console.log('Error in Saving user: '+err);
-                                throw err;
-                            }
-                            console.log('User Registration succesful');
-                            return done(null, newAccount);
-                        });
-                    }
-                });
-            };
-            // Delay the execution of findOrCreateUser and execute the method
-            // in the next tick of the event loop
-            process.nextTick(findOrCreateUser);
-        })
-    );
-
-    // Generates hash using bCrypt
-        var createHash = function(password){
-    				//return password;
-            return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
-        }
 
 app.use('/', profile);
 app.use('/profile', profile);
@@ -222,58 +75,6 @@ app.use('/reports/report67152', report67152);
 app.use('/reports/report672', report672);
 app.use('/reports/report675', report675);
 
-var OpenLdap = require('./modules/openldap');
-var Crypt = require('./modules/crypt_sha');
-var openldap = new OpenLdap();
-var crypt = new Crypt();
-
-var isAuthenticated = function(req, res, next){
-  openldap.authenticate('ro_admin', 'guest1@zflexsoftware.com', function(result){
-    winston.log('info', 'done callback and get back to main');
-    winston.log('info', result);
-    if(result instanceof Error){
-      winston.log('error','Authentication Error');
-      next(result);
-    }
-    else{
-      winston.log('info', 'Authentication Success');
-      req.user = result;
-      next();
-    }
-  });
-
-}
-
-app.use('/testcallback', isAuthenticated, function(req,res,next){
-  res.send('test callback');
-});
-
-
-app.get('/mon_create', function(req,res,next){
-  var cz = new Account({
-    id: 4,
-    username: "cz",
-    password: "123456"
-  });
-
-  cz.dudify(function(err, username){
-    if(err) throw err;
-
-    console.log('New account is ' + username);
-  });
-  cz.save(function(err){
-    if(err) throw err;
-
-    console.log('Username saved successfully!');
-  });
-})
-app.get('/mon_all', function(req, res, next){
-  Account.find({}, function(err,accounts){
-    if(err) throw err;
-
-    res.send(accounts);
-  })
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
